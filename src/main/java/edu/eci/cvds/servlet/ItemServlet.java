f7f9b0267a2acd1cc9fc4a96d1a289d903f5f483
KevinEGP <kevinegp13@gmail.com>
2021-03-04 11:06:47 -0500

    Parte 2 Completa

--- src/main/java/edu/eci/cvds/servlet/ItemServlet.java
+++ src/main/java/edu/eci/cvds/servlet/ItemServlet.java
@@ -1,6 +1,8 @@
 package edu.eci.cvds.servlet;
 
 import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 import java.io.Writer;
 import java.util.Optional;
 import javax.servlet.ServletException;
@@ -13,37 +15,51 @@ import edu.eci.cvds.servlet.model.Todo;
 import java.util.ArrayList;
 
 @WebServlet(
-    urlPatterns = "/BuscarItems"
+    urlPatterns = "/Items"
 )
 public class ItemServlet extends HttpServlet{
     static final long serialVersionUID = 35L;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException,NumberFormatException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, NumberFormatException {
+        
+        int id = 0;
+        ArrayList<Todo> todoList = new ArrayList<Todo>();
         Writer responseWriter = resp.getWriter();
         Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
+
+
         if (!optId.isPresent()){
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             }
         else {
             String id1 = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "";
             try{
-                int id = Integer.parseInt(id1);
+                id = Integer.parseInt(id1);
                 Todo todo = Service.getTodo(id);
+                todoList.add(todo);
+	            String itemsTable = Service.todosToHTMLTable(todoList);
+	            resp.setStatus(HttpServletResponse.SC_OK);
+	            responseWriter.write(itemsTable);
+	            responseWriter.flush();
             }
-            catch(NumberFormatException e){
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);  
+
+            catch(NumberFormatException e0){
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             }
+
             catch(MalformedURLException e1){
                 resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             }
-            ArrayList<Todo> todoList = new ArrayList<Todo>();
-            todoList.add(todo); 
-            String itemTable = Service.todosToHTMLTable(todoList);
-            resp.setStatus(HttpServletResponse.SC_OK);
-            responseWriter.write(itemTable);
-            responseWriter.flush();
-       }
-       
-   }
+
+            catch(FileNotFoundException e3){
+            	resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            	responseWriter.write("Id: " + id + " No encontrado");
+            }
+
+            finally{
+	           	resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);	           	
+            }  
+        }
+    }
 }
