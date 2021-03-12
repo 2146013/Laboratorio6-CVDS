1315a85e433159dc94cac72cd713ec26af8b9f92
KevinEGP <kevinegp13@gmail.com>
2021-03-11 20:50:34 -0500

    Corrección

--- src/main/java/edu/eci/cvds/servlet/Game.java
+++ src/main/java/edu/eci/cvds/servlet/Game.java
@@ -12,25 +12,39 @@ public class Game {
     public int numero;
     public int intentos;
     public int premio;
-    public boolean estado;
+    public String estado;
+    public String mensaje;
+    public ArrayList<Integer> ultimos;
 
     public Game(){
         numero = (int)(Math.random() * ((20 - 0) + 1)) + 0;
         intentos = 0;
         premio = 100000;
-        estado = false;
+        estado = "none";
+        ultimos = new ArrayList<Integer>();
+        setMensaje("Sigue intentando");
     }
 
     public void guess(int num){
 
-        if (!estado) {
+        if (estado == "none") {
             setIntentos(getIntentos()+1);
-            if (num == getNumero() || premio == 0){
-                setEstado(true);
+            ultimos.add(num);
+            if (num == getNumero()){
+            	setEstado("visible");
+            	setMensaje("Ganaste");
             }
+            else if (premio == 0){
+            	setEstado("visible");
+            	setMensaje("Perdiste");
+            }
+
             else {
                 setPremio(getPremio()-reduce);
             }            
+        }
+        else{
+
         }
     }
 
@@ -38,9 +52,14 @@ public class Game {
         setNumero((int)(Math.random() * ((20 - 0) + 1)) + 0);
         setIntentos(0);
         setPremio(100000);
-        setEstado(false);
+        setEstado("none");
+        setUltimos(new ArrayList<Integer>());
+        setMensaje("Sigue intentando");
     }
 
+    public void setUltimos(ArrayList<Integer> ultimos){
+        this.ultimos = ultimos;
+    }
 
     public void setNumero(int numero){
         this.numero = numero;
@@ -54,9 +73,21 @@ public class Game {
         this.premio = premio;
     }
 
-    public void setEstado(boolean estado){
+    public void setMensaje(String mensaje){
+        this.mensaje = mensaje;
+    }
+
+    public void setEstado(String estado){
         this.estado = estado;
     }
+
+	public String getUltimos(){
+		String ult = "";
+		for (Integer i: ultimos) {
+			ult = ult + ((int)i) + ", ";
+		}
+		return ult;
+	}
 
     public int getNumero(){
         return numero;
@@ -70,7 +101,11 @@ public class Game {
         return premio;
     }
 
-    public boolean getEstado(){
+    public String getMensaje(){
+        return mensaje;
+    }
+
+    public String getEstado(){
         return estado;
     }
 }
